6887177a56b6ee107f7d761ca1fa0d27a3e87774
jongsik <kjsik11@gmail.com>
2023-01-17 19:17:26 +0900

    minor fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css';
 
 import { AppProps } from 'next/app';
 
-import CommonLayout from 'component/layout/CommonLayout';
+import CommonLayout from 'components/layout/CommonLayout';
 
 export default function App({ Component, pageProps }: AppProps) {
   const Layout = (Component as any).Layout || CommonLayout;
